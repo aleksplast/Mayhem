a1f7247c650ca491784a95f15e63c902808f4be8
Plastinin Alexander <aleksplast6@gmail.com>
2024-02-21 14:02:15 +0300

    [cards] Deck iterators added, file system restructured     - Added deck iterators     - Fixed some issues with file system

---
+++ include/core/base.h
@@ -0,0 +1,46 @@
+#ifndef BASE_H
+#define BASE_H
+
+#include "card.h"
+#include "deck.h"
+
+namespace Mayhem {
+
+class Base : public Card {
+private:
+    uint32_t power_to_win_;
+    std::array<uint32_t, 3> points_;
+    Deck<Minion> cards_;
+    uint32_t current_power_;
+
+public:
+    void activate_abillity() override {};
+
+    void gain_minion(const Minion* card) {
+        current_power_ += card->get_power();
+
+        cards_.gain_card(card);
+    }
+
+    void remove_minion(const Minion& card) {
+        current_power_ -= card.get_power();
+
+        cards_.remove_card(card);
+    }
+
+    bool is_captured() const {
+        if (current_power_ >= power_to_win_) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+public: //graphic functions
+    Base(int id, std::string ability, uint32_t power_to_win, std::array<uint32_t, 3> points) :
+        Card(id, ability), power_to_win_(power_to_win), points_(points) {};
+}; // class Base
+
+} // namespace Mayhem
+
+#endif // END_H
--- include/core/deck.h
+++ include/core/deck.h
@@ -1,20 +1,52 @@
 #ifndef DECK_H
 #define DECK_H
 
+#include "card.h"
+#include "entity.h"
 #include <cstdint>
 #include <list>
+#include <iostream>
 
 namespace Mayhem {
 
-template <class T>
-class Deck {
+template <class T> class Deck {
 private:
     std::list<T> cards_;
 
-public:
-    void gain_card(const T* card);
-    void remove_card(const T& card);
-    void shuffle();
+public: // Iterator functions
+    using container = std::list<T>;
+    using iterator = typename container::iterator;
+    using const_iterator = typename container::const_iterator;
+
+    iterator begin() { return cards_.begin(); };
+    iterator end() { return cards_.end(); };
+    const_iterator begin() const { return cards_.begin(); };
+    const_iterator end() const { return cards_.end(); };
+
+public: // Interface functions
+    void gain_card(const T* card) {
+        cards_.push_back(*card);
+    }
+    void remove_card(const T& card) {
+        int card_id = card.get_id();
+
+        for (auto it_curr = cards_.begin(), it_end = cards_.end(); it_curr != it_end; ++it_curr) {
+            if (card_id == it_curr->get_id()) {
+                cards_.erase(it_curr);
+                break;
+            }
+        }
+    }
+    std::size_t size() const {
+        return cards_.size();
+    }
+    void shuffle() {
+        cards_.sort(cards_.begin(), cards_.end(), cmp_func());
+    }
+
+int cmp_func() {
+    return std::rand() - RAND_MAX / 2;
+}
 }; // class Deck
 
 } // namespace Mayhem
