30be56f41128ad1a591d681223a15850a98dfe83
Gauch Kirill <kgauch@mail.ru>
2024-02-21 12:36:59 +0300

    [Graphics] draw-function improvement - Corected draw function for Playground

--- include/core/game_core.h
+++ include/core/game_core.h
@@ -1,91 +1,99 @@
 #ifndef GAME_CORE_H
 #define GAME_CORE_H
 
+#include "card.h"
+#include "deck.h"
+#include "entity.h"
 #include <array>
+#include <deque>
+#include <iostream>
 #include <list>
 #include <stdlib.h>
-#include <vector>
 #include <string>
-#include <deque>
-#include "deck.h"
-#include "card.h"
-#include "entity.h"
-#include <iostream>
+#include <vector>
 
 namespace Mayhem {
 
 class Player : public Entity {
 private:
-    Deck<PlayerCard> hand_;
-    Deck<PlayerCard> dump_deck_;
-    Deck<PlayerCard> deck_;
+  Deck<PlayerCard> hand_;
+  Deck<PlayerCard> dump_deck_;
+  Deck<PlayerCard> deck_;
 
-    uint32_t points_;
+  uint32_t points_;
 
 public:
-    Player() = default;
-    Player(Graphics &graphics) : Entity("./src/graphics/images/player.png") { sprite.setColor(sf::Color(0,0,0,100)); };
-    void play_card(PlayerCard* card); //from hand to base, just destroys card in hand, no interaction with the base
-    void end_turn();
-    void dump_card(PlayerCard* card); // card to dump deck
-    void gain_card(); // from deck to hand
-    ~Player() = default;
-    
+  Player() = default;
+  Player(Graphics &graphics) : Entity("./src/graphics/images/player.png") {
+    sprite.setColor(sf::Color(0, 0, 0, 100));
+  };
+  void play_card(PlayerCard *card); // from hand to base, just destroys card in
+                                    // hand, no interaction with the base
+  void end_turn();
+  void dump_card(PlayerCard *card); // card to dump deck
+  void gain_card();                 // from deck to hand
+  ~Player() = default;
+
 public: // graphic function
-    void draw(Graphics &graphic);
+  void draw(Graphics &graphic);
 }; // class Player
 
 class Playground : public Drawable {
 private:
-    Deck<Base*> active_bases_;
-    Deck<Base*> bases_;
-    Deck<Base*> dump_;
+  Deck<Base *> active_bases_;
+  Deck<Base *> bases_;
+  Deck<Base *> dump_;
 
-    std::vector<Player> players_;
+  std::vector<Player> players_;
 
 public:
-    Playground() = delete;
-    Playground(Graphics &graphics) : Drawable("./src/graphics/images/playground.jpg")
-    {
-        auto window_size = graphics.window.getSize();
-        sprite.setPosition(sf::Vector2f(window_size.x / 2, window_size.y / 2));
-        sprite.setScale(static_cast<float>(window_size.x) / texture.getSize().x,
-                        static_cast<float>(window_size.y) / texture.getSize().y);
-        
-        players_.push_back(*(new Player(graphics)));
-        players_.push_back(*(new Player(graphics)));
-        players_.push_back(*(new Player(graphics)));
-        players_.push_back(*(new Player(graphics)));
-    };
-    void clear_base(Base& base); // FIXME: just mock
-    void destroy_base(Base& base);
-    void set_new_base();
-    ~Playground() = default;
+  Playground() = delete;
+  Playground(Graphics &graphics)
+      : Drawable("./src/graphics/images/playground.jpg") {
+    auto window_size = graphics.window.getSize();
+    sprite.setPosition(sf::Vector2f(window_size.x / 2, window_size.y / 2));
+    sprite.setScale(static_cast<float>(window_size.x) / texture.getSize().x,
+                    static_cast<float>(window_size.y) / texture.getSize().y);
+
+    players_.push_back(*(new Player(graphics)));
+    players_.push_back(*(new Player(graphics)));
+    players_.push_back(*(new Player(graphics)));
+    players_.push_back(*(new Player(graphics)));
+  };
+  void clear_base(Base &base); // FIXME: just mock
+  void destroy_base(Base &base);
+  void set_new_base();
+  ~Playground() = default;
+
+private: // graphic function
+  void set_player_posission(Graphics &graphics, uint16_t player_id);
+  void set_rotate(Graphics &graphics, uint16_t player_id);
+  void set_scale(Graphics &graphics, uint16_t player_id);
 
 public: // graphic function
-    void draw(Graphics &graphic);
+  void draw(Graphics &graphic);
 }; // class Playground
 
 class Engine {
 private:
-    Playground playground;
-    int time; // FIXME: just mock
-    int turn; // FIXME: just mock
+  Playground playground;
+  int time; // FIXME: just mock
+  int turn; // FIXME: just mock
 
 public:
-    Engine() = delete;
-    Engine(Graphics &graphics) : playground(graphics) {};
-    void give_points(uint16_t number, uint16_t player_id) {};
-    void give_card(uint16_t id, PlayerCard* card); // так это типо отдать игроку карту, после завершения хода
-    void place_card(uint16_t player_id, PlayerCard* card, uint16_t base);
-    void end_turn(uint16_t player_id);
-    ~Engine() = default;
+  Engine() = delete;
+  Engine(Graphics &graphics) : playground(graphics){};
+  void give_points(uint16_t number, uint16_t player_id){};
+  void give_card(uint16_t id, PlayerCard *card); // так это типо отдать игроку
+                                                 // карту, после завершения хода
+  void place_card(uint16_t player_id, PlayerCard *card, uint16_t base);
+  void end_turn(uint16_t player_id);
+  ~Engine() = default;
 
-public: //graphic functions
-    void draw(Graphics &grafics);
+public: // graphic functions
+  void draw(Graphics &grafics);
 }; // class Engine
 
 } // namespace Mayhem
 
 #endif // GAME_CORE_H
-
--- include/graphics/graphics.hpp
+++ include/graphics/graphics.hpp
@@ -12,43 +12,42 @@ namespace Mayhem {
 
 class Graphics {
 public:
-    enum class GameType
-    {
-        ONLINE,
-        OFLINE
-    };
+  enum class GameType { ONLINE, OFLINE };
 
 private:
-    const float num_cards_per_side_ = 10;
-    const GameType type_;
-    uint16_t draw_player_; // num player who graphic draw
-    const float player_increase_ = 1.8;
-    
+  const float num_cards_per_side_ = 10;
+  const float num_players_per_side_ = 3;
+  const GameType type_;
+  uint16_t draw_player_; // num player who graphic draw
+  const float player_increase_ = 1.8;
+
 public:
-    sf::RenderWindow window;
-    std::vector<PlayerCard*> player_cards;
-    std::vector<Deck<PlayerCard>*> decks;
-    std::vector<Base*> active_bases;
-    
+  sf::RenderWindow window;
+  std::vector<PlayerCard *> player_cards;
+  std::vector<Deck<PlayerCard> *> decks;
+  std::vector<Base *> active_bases;
+
 public:
-    Graphics() = delete;
-    Graphics(GameType type, uint16_t draw_player = 0) : type_(type), draw_player_(draw_player) {};
-    void end_turn();
-    const float get_num_cards_per_side() const;
-    uint16_t get_draw_player() const;
-    const float get_player_increase() const;
-    ~Graphics() = default;
+  Graphics() = delete;
+  Graphics(GameType type, uint16_t draw_player = 0)
+      : type_(type), draw_player_(draw_player){};
+  void end_turn();
+  const float get_num_cards_per_side() const;
+  uint16_t get_draw_player() const;
+  const float get_num_players_per_side() const;
+  const float get_player_increase() const;
+  ~Graphics() = default;
 }; // class Grafics
 
 class Drawable {
 public:
-    sf::Sprite sprite;
-    sf::Texture texture;
-    
+  sf::Sprite sprite;
+  sf::Texture texture;
+
 public:
-    Drawable() = default;
-    Drawable(const std::string &texture_file);
-    ~Drawable() = default;
+  Drawable() = default;
+  Drawable(const std::string &texture_file);
+  ~Drawable() = default;
 }; // class Drawable
 
 } // namespace Mayhem
