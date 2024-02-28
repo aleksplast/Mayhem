7e576f1d0283046fe4abd9b1687e3875c98a88c3
Gauch Kirill <kgauch@mail.ru>
2024-02-28 20:45:07 +0300

    [graphics] Changed the name of the functions

--- src/graphics/draw.cpp
+++ src/graphics/draw.cpp
@@ -8,7 +8,9 @@
 namespace Mayhem {
 
 void Player::draw(Graphics &graphics) // draw cards
-{}
+{
+
+}
 
 void Playground::set_player_position(Graphics &graphics, uint16_t player_id) {
     sf::Vector2f playground_size =
@@ -32,8 +34,8 @@ void Playground::set_player_position(Graphics &graphics, uint16_t player_id) {
     }
 }
 
-void Playground::set_rotate(Graphics &graphics, uint16_t player_id) {
-    auto &curr_sprite = players_[player_id]->sprite;
+void Playground::set_player_rotate(Graphics &graphics, uint16_t player_id) {
+    auto &curr_sprite = players_[player_id].sprite;
     switch (player_id) {
     case 1:
         curr_sprite.setRotation(180);
@@ -49,7 +51,7 @@ void Playground::set_rotate(Graphics &graphics, uint16_t player_id) {
     }
 }
 
-void Playground::set_scale(Graphics &graphics, uint16_t player_id) {
+void Playground::set_player_scale(Graphics &graphics, uint16_t player_id) {
     sf::Vector2f playground_size =
         sf::Vector2f(texture.getSize().x * sprite.getScale().x, texture.getSize().y * sprite.getScale().y);
     auto texture_size = players_[player_id]->texture.getSize();
@@ -73,10 +75,10 @@ void Playground::draw(Graphics &graphics) // draw bases, players
     do {
         uint16_t player_id = (num_players + draw_player - graphics.get_draw_player()) % num_players;
         set_player_position(graphics, player_id);
-        set_rotate(graphics, player_id);
-        set_scale(graphics, player_id);
-        players_[player_id]->draw(graphics);
-        graphics.window.draw(players_[draw_player]->sprite);
+        set_player_rotate(graphics, player_id);
+        set_player_scale(graphics, player_id);
+        players_[player_id].draw(graphics);
+        graphics.window.draw(players_[draw_player].sprite);
         draw_player = (draw_player + 1) % num_players;
     } while (draw_player != graphics.get_draw_player());
 }
