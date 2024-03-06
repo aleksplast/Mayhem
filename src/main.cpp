a5e36b758416357bac4f5a47a7cc7db3e041c0e5
Gauch Kirill <kgauch@mail.ru>
2024-03-06 12:23:06 +0300

    [general] Fixed merge bugs - Added a description of the deleted functions

--- src/main.cpp
+++ src/main.cpp
@@ -9,27 +9,26 @@ using namespace Mayhem;
 
 int main() {
     Graphics graphics(Graphics::GameType::OFLINE, 0);
-    // sf::ContextSettings settings;
-    // settings.antialiasingLevel = 8;
-    // graphics.window.create(sf::VideoMode::getFullscreenModes()[0], "Mayhem", sf::Style::Default, settings);
-    // graphics.window.setFramerateLimit(100);
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    graphics.window.create(sf::VideoMode::getFullscreenModes()[0], "Mayhem", sf::Style::Default, settings);
+    graphics.window.setFramerateLimit(100);
 
     Engine engine (graphics);
     engine.start_game();
     engine.dump_state("dump_file");
 
-    // while (graphics.window.isOpen()) {
-    //     sf::Event event;
-    //     while (graphics.window.pollEvent(event))
-    //         ;
-    //     {
-    //         if (event.type == sf::Event::Closed) {
-    //             graphics.window.close();
-    //         }
-    //     }
-    //     graphics.window.clear();
-    //     engine.draw(graphics);
-    //     graphics.window.display();
-    // }
+    while (graphics.window.isOpen()) {
+        sf::Event event;
+        while (graphics.window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) {
+                graphics.window.close();
+            }
+        }
+        graphics.window.clear();
+        engine.draw(graphics);
+        graphics.window.display();
+    }
     return 0;
 }
