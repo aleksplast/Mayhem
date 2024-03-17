181cfdd803d6a96599239c7040ea1e9ae3925113
Gauch Kirill <kgauch@mail.ru>
2024-03-17 12:08:20 +0300

    [graphics] Formed functions in the process of event parsing

--- src/graphics/graphics.cpp
+++ src/graphics/graphics.cpp
@@ -55,47 +55,53 @@ Deck<PlayerCard *> *Graphics::pressed_deck(const sf::Vector2f &pos) const {
     return nullptr;
 }
 
+void Graphics::get_event_parameters(const sf::Event &event, GameEvent &game_event) const {
+    if (event.type == sf::Event::Closed) {
+        game_event.type = GameEvent::EventType::close_window;
+    } else if (event.type == sf::Event::MouseButtonPressed) {
+        if (event.mouseButton.button == sf::Mouse::Left) {
+            sf::Vector2u window_size = attributes.window.getSize();
+            sf::Vector2f mouse_pos =
+                sf::Vector2f(event.mouseButton.x * attributes.default_window_size.x / window_size.x,
+                             event.mouseButton.y * attributes.default_window_size.y / window_size.y);
+            if (!game_event.card)
+                game_event.card = pressed_card(mouse_pos);
+
+            if (!game_event.base)
+                game_event.base = pressed_base(mouse_pos);
+            if (!game_event.base)
+                game_event.deck = pressed_deck(mouse_pos);
+        }
+    } else if (event.type == sf::Event::KeyPressed) {
+        if (event.key.code == sf::Keyboard::Enter) {
+            game_event.type = GameEvent::EventType::end_turn;
+        }
+    }
+    set_event_type(game_event);
+}
+
+void Graphics::set_event_type(GameEvent &game_event) const {
+    if (game_event.card) {
+        if (game_event.base)
+            game_event.type = GameEvent::EventType::place_card_to_base;
+        if (game_event.deck)
+            game_event.type = GameEvent::EventType::place_card_to_deck;
+        if (!game_event.base && !game_event.deck)
+            game_event.type = GameEvent::EventType::wait_event;
+    } else {
+        if (game_event.base)
+            game_event.type = GameEvent::EventType::show_base;
+        if (game_event.deck)
+            game_event.type = GameEvent::EventType::show_deck;
+    }
+}
+
 void Graphics::parse_events(GameEvent &game_event) {
-    game_event.type = GameEvent::EventType::no_event;
+    game_event = GameEvent(GameEvent::EventType::no_event, nullptr, nullptr, nullptr);
     sf::Event event;
 
     while (attributes.window.pollEvent(event) || game_event.type == GameEvent::EventType::wait_event) {
-        if (event.type == sf::Event::Closed) {
-            game_event.type = GameEvent::EventType::close_window;
-            break;
-        }
-        if (event.type == sf::Event::MouseButtonPressed) {
-            if (event.mouseButton.button == sf::Mouse::Left) {
-                sf::Vector2u window_size = attributes.window.getSize();
-                sf::Vector2f abs_mouse_pos = sf::Vector2f(event.mouseButton.x * attributes.default_window_size.x / window_size.x,
-                                                      event.mouseButton.y * attributes.default_window_size.y / window_size.y);
-                if (!game_event.card)
-                    game_event.card = pressed_card(abs_mouse_pos);
-
-                if (!game_event.base)
-                    game_event.base = pressed_base(abs_mouse_pos);
-                if (!game_event.base)
-                    game_event.deck = pressed_deck(abs_mouse_pos);
-            }
-        }
-        if (event.type == sf::Event::KeyPressed) {
-            if (event.key.code == sf::Keyboard::Enter) {
-                game_event.type = GameEvent::EventType::end_turn;
-            }
-        }
-
-        if (game_event.card) {
-            if (game_event.base)
-                game_event.type = GameEvent::EventType::place_card_to_base;
-            if (game_event.deck)
-                game_event.type = GameEvent::EventType::place_card_to_deck;
-            if (!game_event.base && !game_event.deck) game_event.type = GameEvent::EventType::wait_event;
-        } else {
-            if (game_event.base)
-                game_event.type = GameEvent::EventType::show_base;
-            if (game_event.deck)
-                game_event.type = GameEvent::EventType::show_deck;
-        }
+        get_event_parameters(event, game_event);
     }
 }
 
@@ -104,12 +110,10 @@ void Graphics::process_events(Engine &engine) {
     parse_events(game_event);
     if (game_event.type == GameEvent::EventType::close_window)
         attributes.window.close();
-    else
-    if (game_event.type == GameEvent::EventType::place_card_to_base) {
+    else if (game_event.type == GameEvent::EventType::place_card_to_base) {
         engine.place_card(attributes.draw_player, game_event.card->get_id(), game_event.base->get_id());
         game_event = GameEvent();
-    } else
-    if (game_event.type == GameEvent::EventType::end_turn) {
+    } else if (game_event.type == GameEvent::EventType::end_turn) {
         engine.end_turn(attributes.draw_player);
         game_event = GameEvent();
     }
