f48d58a94b33c2642d207082da0d2c70d0b045f4
Gauch Kirill <kgauch@mail.ru>
2024-05-06 00:49:35 +0300

    [Icon] Add game's icon

--- src/graphics/graphics_model.cpp
+++ src/graphics/graphics_model.cpp
@@ -1,4 +1,5 @@
 #include "graphics/graphics_model.hpp"
+#include <SFML/Graphics/Image.hpp>
 
 namespace Mayhem {
 
@@ -13,6 +14,12 @@ GraphicsModel::GraphicsModel(Engine &e) : engine(e) {
 
     attributes.window.create(video_mode, "Mayhem", sf::Style::Default, settings);
     attributes.window.setFramerateLimit(100);
+
+    sf::Image icon;
+    if (icon.loadFromFile("../assets/images/icon")) {
+        sf::Vector2u icon_size = icon.getSize();
+        attributes.window.setIcon(icon_size.x, icon_size.y, icon.getPixelsPtr());
+    }
 }
 
 } // namespace Mayhem
